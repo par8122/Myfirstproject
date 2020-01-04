25e3668f40951747c278f19843443b182d3ef3c5
par8122 <59494737+par8122@users.noreply.github.com>
2020-01-04 14:16:05 -0600

    Update Shortcut.py

--- Shortcut.py
+++ Shortcut.py
@@ -12,16 +12,7 @@ class Graph():
         e.g. {('X', 'A'): 7, ('X', 'B'): 2, ...}
         """
         self.edges = defaultdict(list)
-        self.weights = {}
-    
-    def add_edge(self, from_node, to_node, weight):
-        # Note: assumes edges are bi-directional
-        self.edges[from_node].append(to_node)
-        self.edges[to_node].append(from_node)
-        self.weights[(from_node, to_node)] = weight
-        self.weights[(to_node, from_node)] = weight
-
-graph = Graph()
+        graph = Graph()
 
 edges = [
     ('X', 'A', 7),
