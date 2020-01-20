986f256ac5b3acbbb1a567f4fd5ba7fb12d21a88
par8122 <59494737+par8122@users.noreply.github.com>
2020-01-20 14:11:53 -0600

    Update Shortcut.py

--- Shortcut.py
+++ Shortcut.py
@@ -4,13 +4,7 @@ from collections import defaultdict
 
 class Graph():
     def __init__(self):
-        """
-        self.edges is a dict of all possible next nodes
-        e.g. {'X': ['A', 'B', 'C', 'E'], ...}
-        self.weights has all the weights between two nodes,
-        with the two nodes as a tuple as the key
-        e.g. {('X', 'A'): 7, ('X', 'B'): 2, ...}
-        """
+        
         self.edges = defaultdict(list)
         graph = Graph()
 
